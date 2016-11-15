9d5feae87cd4dba127e46ea8fe10409c8dfe1bd2
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-11-14 18:59:53 -0800

    show page styling

---
+++ app/helpers/_web_scraper.rb
@@ -0,0 +1,9 @@
+require 'HTTParty'
+require 'Nokogiri'
+require 'JSON'
+require 'pry'
+
+def scrape_site(site_url)
+  page = HTTParty.get(site_url)
+  binding.pry
+end
--- app/helpers/polish_helper.rb
+++ app/helpers/polish_helper.rb
@@ -1,2 +1,4 @@
 module PolishHelper
+
+  
 end
